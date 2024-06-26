582dc161e747f2ed88d47cb4b5a370a6c9ae3d36
lezhnevay <lezhnevayy@yandex.ru>
2024-06-26 10:34:18 +0500

    1. Add test http.StatusOK for TestMainHandlerWhenCountMoreThenTotal; 2. Replaced assert for TestMainHandlerWhenMissingCount and TestMainHandlerWhenOK; 3. Delete bad comments.

--- precode_test.go
+++ precode_test.go
@@ -7,16 +7,18 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
+	assert.Equal(t, responseRecorder.Code, http.StatusOK)
 	countCafe := strings.Split(responseRecorder.Body.String(), ",")
 	assert.Equal(t, len(countCafe), totalCount)
 }
@@ -28,8 +30,8 @@ func TestMainHandlerWhenOk(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.NotEmpty(t, responseRecorder)
-	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+	assert.NotEmpty(t, responseRecorder.Body)
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 }
 
 func TestMainHandlerWhenMissingCount(t *testing.T) {
@@ -39,12 +41,6 @@ func TestMainHandlerWhenMissingCount(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
 	assert.Equal(t, responseRecorder.Body.String(), "wrong city value")
 }
-
-// здесь нужно добавить необходимые проверки
-
-//func TestMainHandlerWrongCityValue(t *testing.T) {
-
-//}
